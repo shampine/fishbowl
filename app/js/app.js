9aee18a9ae6a7fef17b79420bcd787e263be1729
patrickshampine <patrick@patrickshampine.com>
2014-07-21 22:36:42 -0700

    Pulled RT data, added sample config, displayed title && thumb

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,29 @@
-var test = 123;
+(function() {
+
+  var app = angular.module('fishbowl', ['fishbowl.config']);
+
+  app.controller('CatalogController', [ '$http', 'CONFIG', function($http, CONFIG) {
+    var catalog = this;
+
+    catalog.movies = [];
+
+    catalogUrl = 'http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/current_releases.json';
+
+    $http.jsonp(catalogUrl, {
+        params: {
+            apikey: CONFIG.ROTTEN_TOMATOES,
+            callback: 'JSON_CALLBACK'
+        }
+    })
+    .success(function (data) {
+  
+        catalog.movies = data;
+        catalog.movies = data.movies;
+        console.log(catalog.movies);
+
+    });
+
+  }]);
+
+
+})();
---
+++ app/js/config.sample.js
@@ -0,0 +1,6 @@
+var config_module = angular.module('fishbowl.config', [])
+  .constant('CONFIG', {
+    'APP_NAME': 'fishbowl',
+    'APP_VERSION': '0.0.1',
+    'ROTTEN_TOMATOES': ''
+});
